c0366ea2bf4a1dd7b30db4ce43af35dd56f50096
LAPTOP-BGAV0320\ramse <idv18c.rguerrero@uartesdigitales.edu.mx>
2019-05-16 22:24:53 -0500

    .cpp: Eliminación de punteros, se cambió por vectores, se agregó una bandera y se mejoró el programa .h: Eliminación de dos librerias sin uso alguno

--- Parcial 1/Metodo_Burbuja/Programa_Burbuja.cpp
+++ Parcial 1/Metodo_Burbuja/Programa_Burbuja.cpp
@@ -2,12 +2,14 @@
 
 using namespace std;
 
-int* burbuja(int* cant, int longitud)
+vector <int> burbuja(vector <int> cant, int longitud)
 {
 	int r1 = 0;
+	bool bandera;
 	//Ciclo para acomodar
-	for (int i = 0; i < longitud; i++)
+	for (int i = longitud - 1; i > 0; i--)
 	{
+		bandera = false;
 		for (int j = 0; j < longitud - 1; j++)
 		{
 			if (cant[j] > cant[j + 1])
@@ -15,33 +17,38 @@ int* burbuja(int* cant, int longitud)
 				r1 = cant[j];
 				cant[j] = cant[j + 1];
 				cant[j + 1] = r1;
+				bandera = true;
 			}
+		}
+		if (bandera == false)//Condición en caso de que el algortimos ya este ordenado
+		{
+			break;
 		}
 	}
 	return cant;
 }
+
 int main()
 {
 	unsigned char valor;
 	unsigned int longi = 0;
-	int* final;
+	vector <int> final;
 
 	cout << "Ingresa 1 para iniciar" << endl;
 	cin >> valor;
-
 	if (valor == '1')
 	{
 		cout << "Ingresa la longitud" << endl;
 		cin >> longi;
-		int* datos = new int[longi];
+		vector <int> datos(longi);
 		cout << "Ingresa los datos" << endl;
 		//Ciclo para ingresar datos
-		for (int i = 0; i < longi; i++)
+		for (int i = 0; i < size(datos); i++)
 		{
 			cin >> datos[i];
 		}
 		final = burbuja(datos, longi);
-		for (int i = 0; i < longi; i++)
+		for (int i = 0; i < final.size(); i++)
 		{
 			cout << final[i] << " ";
 		}
